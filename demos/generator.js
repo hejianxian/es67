e943254a6d9be24fcfc9c76d480bf78d4a8d6212
Hejx <531601727@qq.com>
2017-06-04 23:18:07 +0800

    Add generator promise

--- demos/generator.js
+++ demos/generator.js
@@ -1,28 +1,55 @@
-// 生成器的主要功能是： 通过一段程序，持续迭代或枚举出符合某个公式或算法的有序数列中的元素。
+// generator promise
 
-// 传统的编程语言，早有异步编程的解决方案（其实是多任务的解决方案）。其中有一种叫做"协程"（coroutine），意思是多个线程互相协作，完成异步任务。
+const log = require('../utils/log');
 
-function* getTicket(username){
-  yield setTimeout(() => {
-    return `ticket-${username}`;
-  }, 500);
+const runner = function(generator, value) {
+  const result = generator.next(value);
+  if (result.done) return;
+  result.value.then((val) => {
+    runner(generator, val);
+  })
 };
 
-function* getUserInfo() {
-  yield setTimeout(() => {
-    return {
+const getTicket = function(username) {
+  return new Promise(function(resolve, reject) {
+    resolve(`ticket-${username}`);
+  });
+};
+
+const getUserInfo = function() {
+  return new Promise(function(resolve, reject) {
+    const userInfo = {
       username: 'hejx'
     };
-  }, 1000);
+    resolve(userInfo);
+  });
 };
 
-function* gen() {
-  let userInfo = yield* getUserInfo();
-  let ticket = yield* getTicket(userInfo.value.username);
-  
-  return ticket;
+const delay = function(ms) {
+	return new Promise(function(resolve, reject) {
+		log('delay, ms:', ms);
+		setTimeout(() => {
+				log('delay: done');
+				resolve();
+			}, ms);
+	});
 };
 
-const g = gen();
-console.log(g.next();
+const generatorFunction = function* () {
+  try {
+    const userInfo = yield getUserInfo();
+    yield delay(2000);
+    const ticket = yield getTicket(userInfo.username);
+    log(ticket);
+  } catch (e) {
+    log(e.message);
+  }
+};
 
+const genertor = generatorFunction();
+runner(genertor);
+
+/*
+生成器的主要功能是： 通过一段程序，持续迭代或枚举出符合某个公式或算法的有序数列中的元素。
+传统的编程语言，早有异步编程的解决方案（其实是多任务的解决方案）。其中有一种叫做"协程"（coroutine），意思是多个线程互相协作，完成异步任务。
+*/
