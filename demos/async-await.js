46c5fd0658575bea805844502cfc93c26b75543b
Hejx <531601727@qq.com>
2017-06-07 17:48:24 +0800

    Add classes demos

--- demos/async-await.js
+++ demos/async-await.js
@@ -1,3 +1,15 @@
+// The async keyword before a function has two effects:
+// 
+// Makes it always return a promise.
+// Allows to use await in it.
+// The await keyword before a promise makes JavaScript wait until that promise settles, and then:
+// 
+// If it’s an error, the exception is generated, same as if throw error were called at that very place.
+// Otherwise, it returns the result, so we can assign it to a value.
+// Together they provide a great framework to write asynchronous code that is easy both to read and write.
+// 
+// With async/await we rarely need to write promise.then/catch, but we still shouldn’t forget that they are based on promises, because sometimes (e.g. in the outmost scope) we have to use these methods. Also Promise.all is a nice thing to wait for many tasks simultaneously.
+
 const log = require('../utils/log');
 
 const getTicket = async function(username) {
@@ -18,10 +30,12 @@ const getUserInfo = async function() {
 };
 
 const run = async function() {
+  log('get userInfo');
   let userInfo = await getUserInfo();
+  log('get ticket');
   let ticket = await getTicket(userInfo.username);
   
-  log(ticket);
+  log(`ticket is: ${ticket}`);
 };
 
 run();
---
+++ demos/promise.js
@@ -0,0 +1,5 @@
+// 1: Promise.resolve(value) – makes a resolved promise with the given value,
+// 2: Promise.reject(error) – makes a rejected promise with the given error,
+// 3: Promise.all(promises) – waits for all promises to resolve and returns an array of their results. If any of the given promises rejects, then it becomes the error of Promise.all, and all other results are ignored.
+// 4: Promise.race(promises) – waits for the first promise to settle, and its result/error becomes the outcome.
+
